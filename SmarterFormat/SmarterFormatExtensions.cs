248f87adb96f4bff27a052af93e9fae531f0b36e
hamedfathi <hamedfathi@outlook.com>
2022-05-02 00:23:06 +0200

    feat(Project): Skeleton added.

---
+++ SmarterFormat/SmarterFormatExtensions.cs
@@ -0,0 +1,54 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+using System.Text;
+using SmartFormat;
+
+namespace SmarterFormat;
+
+public static class SmarterFormat
+{
+    public static void AppendSmartLineWithTab(this StringBuilder builder, string value, int counter, params object[] args)
+    {
+        if (counter <= 0)
+        {
+            builder.AppendLine(Smart.Format(value,args));
+        }
+        else
+        {
+            builder.AppendLine($"{new string('\t', counter)}{Smart.Format(value,args)}");
+        }
+    }
+    public static void AppendSmartLineWithWhiteSpace(this StringBuilder builder, string value, int counter, params object[] args)
+    {
+        if (counter <= 0)
+        {
+            builder.AppendLine(Smart.Format(value,args));
+        }
+        else
+        {
+            builder.AppendLine($"{new string(' ', counter)}{Smart.Format(value,args)}");
+        }
+    }
+    public static void AppendSmartWithTab(this StringBuilder builder, string value, int counter, bool tab = true, params object[] args)
+    {
+        if (counter <= 0)
+        {
+            builder.Append(Smart.Format(value,args));
+        }
+        else
+        {
+            builder.Append($"{new string('\t', counter)}{Smart.Format(value,args)}");
+        }
+    }
+    public static void AppendSmartWithWhiteSpace(this StringBuilder builder, string value, int counter, bool tab = true, params object[] args)
+    {
+        if (counter <= 0)
+        {
+            builder.Append(Smart.Format(value,args));
+        }
+        else
+        {
+            builder.Append($"{new string(' ', counter)}{Smart.Format(value,args)}");
+        }
+    }
+}
